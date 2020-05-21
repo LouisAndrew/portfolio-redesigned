7041d69caf7c96dfee0defd40ee715685540502d
Louis <louisandrew3@gmail.com>
2020-05-21 16:25:30 +0200

    created button component and added decorations on hero. 🍌
    // TODO: adjust font-size for different res

---
+++ src/components/Button.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Butt = styled.button`
+        
+        outline: none;
+        padding: .5vw 1vw;
+        border: none;
+        width: fit-content;
+
+        background: none;
+        border-radius: 10px;
+
+        background-color: ${props => {
+
+                switch ( props.$bColor ) {
+                        case 'bg':
+                                return props.theme.bg
+                        case 'ft':
+                                return props.theme.ft
+                        case 'dark':
+                                return props.theme.dark
+                        case 'red':
+                                return props.theme.red
+                        case 'blue':
+                                return props.theme.blue
+                        default:
+                                return props.$bColor
+                }
+        }};
+
+        color: ${props => {
+
+                switch ( props.$color ) {
+                        case 'bg':
+                                return props.theme.bg
+                        case 'ft':
+                                return props.theme.ft
+                        case 'dark':
+                                return props.theme.dark
+                        case 'red':
+                                return props.theme.red
+                        case 'blue':
+                                return props.theme.blue
+                        default:
+                                return props.$color
+                }
+        }};
+`
+
+const Button = props => {
+
+        return (
+                // eslint-disable-next-line react/destructuring-assignment
+                <Butt {...props} $bColor={props.bColor} $color={props.color} >
+                        {
+                                // eslint-disable-next-line react/destructuring-assignment
+                                props.children
+                        }
+                </Butt>
+        )
+}
+
+Button.propTypes = {
+
+        bColor: PropTypes.string,
+        color: PropTypes.string,
+        children: PropTypes.node.isRequired,
+}
+
+Button.defaultProps = {
+
+        bColor: 'def',
+        color: 'def',
+}
+
+export default Button
--- src/components/index-page/Hero.jsx
+++ src/components/index-page/Hero.jsx
@@ -2,17 +2,70 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
+import Laser from '../../../static/assets/decorations/LaserBeam.svg'
+import Hooman from '../../../static/assets/decorations/Hooman.svg'
+import Button from '../Button'
+
 const Container = styled.section`
         
         min-height: 100vh;
+        position: relative;
         background-color: ${({ theme }) => theme.bg};
 
         display: flex;
+
+        #laser {
+
+                position: absolute;
+                right: 0;
+        }
+
+        @media screen and ( max-width: 464px ) {
+                
+                overflow-x: hidden;
+                flex-direction: column;
+                justify-content: center;
+
+                #laser {
+
+                        transform: scale(.7);
+                        right: -7%;
+                }
+
+                #hooman {
+
+                        display: none;
+                }
+        }
 `
 
 const Items = styled.div`
         
         width: 50%;
+        padding-right: 5%;
+
+        z-index: 2;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        &:nth-child(2) {
+                
+                padding-right: 0;
+                align-items: center;
+        }
+
+        @media screen and ( max-width: 464px ) {
+                
+                width: 100%;
+                padding-right: 0;
+
+                align-items: center;
+
+                h1, p {
+                        text-align: center;
+                }
+        }
 `
 
 const Hero = ({ heading, subheading, cta }) => {
@@ -22,8 +75,14 @@ const Hero = ({ heading, subheading, cta }) => {
                         <Items>
                                 <h1>{heading}</h1>
                                 <p>{subheading}</p>
+                                <Button color='ft' bColor='red' >
+                                        {cta}
+                                </Button>
                         </Items>
-                        <Items></Items>
+                        <Items>
+                                <Hooman id='hooman' />
+                        </Items>
+                        <Laser id='laser' />
                 </Container>
           )
 }
