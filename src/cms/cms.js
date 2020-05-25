1022a271bd2c18cb082bd7bd9ba820012524c803
Louis <louisandrew3@gmail.com>
2020-05-25 12:56:29 +0200

    adas

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,5 +3,5 @@ import CMS from 'netlify-cms-app'
 import IndexPagePreview from './preview-templates/index-page-preview'
 import TechPreview from './preview-templates/tech-preview'
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('technologies', TechPreview)
+// CMS.registerPreviewTemplate('index', IndexPagePreview)
+// CMS.registerPreviewTemplate('technologies', TechPreview)
